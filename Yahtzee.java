74a538f9e252362229fef67d22732c27e1e47e19
Skylar Scorca <skylar.scorca@outlook.com>
2022-11-24 18:48:20 -0500

    added scorecards

--- Yahtzee.java
+++ Yahtzee.java
@@ -9,6 +9,7 @@ import java.util.Random;
 import javax.swing.JPanel;
 import java.awt.Graphics;
 import java.awt.Color;
+import java.awt.GridLayout;
 
 public class Yahtzee
 {
@@ -26,6 +27,8 @@ public class Yahtzee
 
 class YahtzeePanel extends JPanel implements ActionListener
 {
+    JPanel scorecard1, scorecard2;
+
     public YahtzeePanel()
     {
         start_game();
@@ -33,7 +36,44 @@ class YahtzeePanel extends JPanel implements ActionListener
 
     public void start_game()
     {
+        scorecard1 = new JPanel();
+        scorecard2 = new JPanel();
+        score_init(scorecard1);
+        score_init(scorecard2);
+        setLayout(new FlowLayout());
+        add(scorecard1);
+        add(scorecard2);
+    }
 
+    public void score_init(JPanel panel)
+    {
+        //panel = new JPanel();
+        panel.setLayout(new GridLayout(13, 2));
+        panel.add(new JLabel("Aces"));
+        panel.add(new JLabel(""));
+        panel.add(new JLabel("Twos"));
+        panel.add(new JLabel(""));
+        panel.add(new JLabel("Threes"));
+        panel.add(new JLabel(""));
+        panel.add(new JLabel("Fours"));
+        panel.add(new JLabel(""));
+        panel.add(new JLabel("Fives"));
+        panel.add(new JLabel(""));
+        panel.add(new JLabel("Sixes"));
+        panel.add(new JLabel(""));
+        panel.add(new JLabel("3 of a kind"));
+        panel.add(new JLabel(""));
+        panel.add(new JLabel("4 of a kind"));
+        panel.add(new JLabel(""));
+        panel.add(new JLabel("Full House"));
+        panel.add(new JLabel(""));
+        panel.add(new JLabel("Small Straight"));
+        panel.add(new JLabel(""));
+        panel.add(new JLabel("Large Straight"));
+        panel.add(new JLabel(""));
+        panel.add(new JLabel("YAHTZEE"));
+        panel.add(new JLabel(""));
+        panel.add(new JLabel("Chance"));
     }
 
     public void actionPerformed(ActionEvent e)
