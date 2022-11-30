5d63e9013dfe8370e61b8c97d9d40bf2fae90eb3
Alec Tremblay <tremblayalec14@gmail.com>
2022-11-30 12:11:01 -0500

    initial refactor, needs more work

--- Yahtzee.java
+++ Yahtzee.java
@@ -16,6 +16,7 @@ import javax.swing.ImageIcon;
 
 public class Yahtzee
 {
+
    public static void main( String args[] )
    { 
       JFrame frame = new JFrame("Yahtzee");
@@ -23,73 +24,32 @@ public class Yahtzee
 
       YahtzeePanel yp = new YahtzeePanel(); 
       frame.add(yp);
-      frame.setSize(500, 500); // set frame size
+      frame.setSize(700, 500); // set frame size
       frame.setVisible(true); // display frame
    }
 }
 
 class YahtzeePanel extends JPanel implements ActionListener
 {
-    JPanel scorecard1, scorecard2;
-    JToggleButton dice[];
+
+    Scoresheet scoresheet;
+    Dice dice;
+    Integer turn, round;
+    JButton roll, play;
 
     public YahtzeePanel()
     {
-        start_game();
-    }
+        //start_game();
+        scoresheet = new Scoresheet(2);
+        dice = new Dice();
+        turn = 3;
+        round = 1;
+        roll = new JButton();
+        play = new JButton();
 
-    public void start_game()
-    {
-        scorecard1 = new JPanel();
-        scorecard2 = new JPanel();
-        score_init(scorecard1);
-        score_init(scorecard2);
-        dice_init();
         setLayout(new FlowLayout());
-        add(scorecard1);
-        add(scorecard2);
-        for(int i = 0; i < 5; i++){
-            add(dice[i]);
-        }
-    }
-
-    public void score_init(JPanel panel)
-    {
-        panel.setLayout(new GridLayout(13, 2));
-        panel.add(new JLabel("Aces"));
-        panel.add(new JLabel(""));
-        panel.add(new JLabel("Twos"));
-        panel.add(new JLabel(""));
-        panel.add(new JLabel("Threes"));
-        panel.add(new JLabel(""));
-        panel.add(new JLabel("Fours"));
-        panel.add(new JLabel(""));
-        panel.add(new JLabel("Fives"));
-        panel.add(new JLabel(""));
-        panel.add(new JLabel("Sixes"));
-        panel.add(new JLabel(""));
-        panel.add(new JLabel("3 of a kind"));
-        panel.add(new JLabel(""));
-        panel.add(new JLabel("4 of a kind"));
-        panel.add(new JLabel(""));
-        panel.add(new JLabel("Full House"));
-        panel.add(new JLabel(""));
-        panel.add(new JLabel("Small Straight"));
-        panel.add(new JLabel(""));
-        panel.add(new JLabel("Large Straight"));
-        panel.add(new JLabel(""));
-        panel.add(new JLabel("YAHTZEE"));
-        panel.add(new JLabel(""));
-        panel.add(new JLabel("Chance"));
-    }
-
-    public void dice_init()
-    {
-        dice = new JToggleButton[5];
-        for(int i = 0; i < 5; i++){
-            Icon initial_face = new ImageIcon(getClass().getResource("Face_1.png"));
-            dice[i] = new JToggleButton(initial_face);
-        }
+        add(scoresheet.panel);
+        add(dice.panel);
     }
 
     public void actionPerformed(ActionEvent e)
